c33795c211cb009634fa1da60532691e1ededcf4
ChEmunah <chanamuna1@gmail.com>
2018-11-18 16:56:39 +0200

    commit111

--- yad-vashem/src/tempMap.js
+++ yad-vashem/src/tempMap.js
@@ -64,6 +64,7 @@ export class CurrentMap extends Component {
 &markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318
 &markers=color:red%7Clabel:C%7C40.718217,-73.998284
 &key=YOUR_API_KEY */}
+
 {
    }
             </div>
